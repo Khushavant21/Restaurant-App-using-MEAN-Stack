a813dd152695eb071c0ecd34ed36db1f10c6393e
unknown <khushawantwagh2002@gmail.com>
2024-03-22 12:47:27 +0530

    add the files

---
+++ src/container/Findus/FindUs.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+
+const FindUs = () => (
+  <div className="app__bg app__wrapper section__padding" id="contact">
+    <div className="app__wrapper_info">
+      <SubHeading title="Contact" />
+      <h1 className="headtext__cormorant" style={{ marginBottom: '3rem' }}>Find Us</h1>
+      <div className="app__wrapper-content">
+        <p className="p__opensans">546, MG Road, Bangalore 5600 32, India</p>
+        <p className="p__cormorant" style={{ color: '#DCCA87', margin: '2rem 0' }}>Opening Hours</p>
+        <p className="p__opensans">Mon - Fri: 11:00 am - 02:00 am</p>
+        <p className="p__opensans">Sat - Sun: 10:00 am - 03:00 am</p>
+      </div>
+      <a href="https://www.google.com/maps/@12.9755388,77.6050886,19z" target="_blank" rel="noopener noreferrer"><button type="button" className="custom__button" style={{ marginTop: '2rem' }}>Open Location</button></a>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.findus} alt="finus_img" />
+    </div>
+  </div>
+);
+
+export default FindUs;
